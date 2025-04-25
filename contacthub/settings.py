5dc183e3d433bef7965f813e22aa6ae219f6752f
Dhiraj <dhirajdurgade7758@gmail.com>
2025-04-26 01:10:38 +0530

    database setup

--- contacthub/settings.py
+++ contacthub/settings.py
@@ -72,21 +72,21 @@ WSGI_APPLICATION = 'contacthub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://postgres:postgres@localhost:5432/mysite',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default="postgresql://contacthub_user:DHSS3NOByG7RhnZHJynT8c3PI95OcUVB@dpg-d05u2phr0fns73enl7dg-a/contact_db_myja",
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
